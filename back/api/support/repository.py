88b617cd067af3334bf8571cf9e20ba9760e39d2
drejon <drc.working@gmail.com>
2024-10-13 19:35:20 +0200

    Create Repositort for demo

---
+++ back/api/support/repository.py
@@ -0,0 +1,19 @@
+class Repository():
+    status =  {'data': [
+        {
+            'name': 'Kika',
+            'age': 10,
+            'breed': 'half breed',
+            'passport': True,
+            'chip': True,
+        }
+    ]}
+
+    @staticmethod
+    def show():
+
+        return Repository.status
+
+    @staticmethod
+    def create(new_animal):
+        Repository.status['data'].append(new_animal)
