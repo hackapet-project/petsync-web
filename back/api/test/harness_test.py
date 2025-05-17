0cdad9506e683655bbd474206597c7c690a6b0e1
Ignacio Delgado <me@igdel.com>
2025-05-17 18:51:19 +0200

    CLEAN START

--- back/api/test/harness_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestHarness(unittest.TestCase):
-
-    def _test_harness(self):
-        self.assertTrue(True)
-
-    def _test_equal(self):
-        self.assertEqual(['foo'], ['foo'])
